1047681fa08f210972a1b67e5c8760a52ca63bc8
Scott Clements <clemencyoflee@gmail.com>
2023-08-19 17:47:32 -0400

    Initial template structure with base.html template and home and about_me templates

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -2,13 +2,18 @@ from flask import Flask, request
 from config import Config
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
 from myapp import routes
 
-@babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+
 
--- myapp/routes.py
+++ myapp/routes.py
@@ -2,5 +2,9 @@ from flask import render_template
 from myapp import app
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/about-me')
+def about_me():
+    return render_template('about_me.html')
