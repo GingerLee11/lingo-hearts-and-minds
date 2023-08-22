36c00dab2f0002bd52c785ca3a352c211dfd860a
Scott Clements <clemencyoflee@gmail.com>
2023-08-22 00:23:19 -0400

    Detail pages set up and pictures added for all projects

--- myapp/routes.py
+++ myapp/routes.py
@@ -8,3 +8,23 @@ def home():
 @app.route('/about-me')
 def about_me():
     return render_template('about_me.html')
+
+@app.route('/charlemos_charlotte')
+def charlemos_charlotte():
+    return render_template('charlemos_charlotte.html')
+
+@app.route('/living_archives')
+def living_archives():
+    return render_template('living_archives.html')
+
+@app.route('/conexiones_que_cuentan')
+def conexiones_que_cuentan():
+    return render_template('conexiones_que_cuentan.html')
+
+@app.route('/southend_spanish')
+def southend_spanish():
+    return render_template('southend_spanish.html')
+
+@app.route('/lhcc')
+def lhcc():
+    return render_template('lhcc.html')
